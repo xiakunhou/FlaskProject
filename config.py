f4079c1474e0871f8e8f50c8715d4a6be4eac48b
xiaokun <xiaokunhou@gmail.com>
2015-04-01 22:44:53 +0800

    add heroku postgresql support

--- config.py
+++ config.py
@@ -4,4 +4,10 @@ CSRF_ENABLED=True
 SECRET_KEY='you-will-nerver-guess'
 
 basedir=os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+#SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
+                               '?check_same_thread=False')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
