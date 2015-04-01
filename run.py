4311637cd02e2ddc9e5887e4c689725df3f2c375
xiaokun <xiaokunhou@gmail.com>
2015-04-01 21:51:49 +0800

    import os

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from iInvest import app
+import os
 
 port = int(os.environ.get('PORT', 5000))
 app.debug=True
-if __name__=='__main__':
-	app.run(host='0.0.0.0',port=port)
+#if __name__=='__main__':
+app.run(host='0.0.0.0',port=port)
