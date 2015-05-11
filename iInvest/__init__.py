7c7dda9e4f8e7573ecef4ad1fd4b83aa49a3231c
xiaokun <xiaokunhou@gmail.com>
2015-05-11 23:03:11 +0800

    merge

--- iInvest/__init__.py
+++ iInvest/__init__.py
@@ -5,23 +5,10 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CsrfProtect
 from flask.ext.bcrypt import Bcrypt
-<<<<<<< HEAD
 #from flask_babelex import Babel
-import flask_login as login
 from flask.ext.babel import Babel
-from flask import Flask, url_for, redirect, render_template, request, session
-#import flask_admin as admin
 
 app=Flask(__name__,template_folder='templates')
-=======
-#from flask.ext.babelex import Babel
-from flask.ext.babel import Babel
-#import flask_admin as admin
-
-app=Flask(__name__,template_folder='templates')
-app.config.from_pyfile('setting.cfg')
-babel= Babel(app)
->>>>>>> 62cf15a7590404335e2a29bf4db2fb33744f5d8a
 
 app.config.from_object('config')
 csrf=CsrfProtect(app)
